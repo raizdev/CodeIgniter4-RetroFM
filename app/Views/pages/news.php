b771e2603174c6b92bd6e2e06b70210b3d631c06
root <root@vps-97c73929.vps.ovh.net>
2021-10-10 18:20:11 +0000

    news array to object

--- app/Views/pages/news.php
+++ app/Views/pages/news.php
@@ -10,7 +10,7 @@
                <div class="news-author">
                   <div class="badge-name">
                      <img class="user-icon" src="img/icon/duck.gif" data-toggle="tooltip" data-placement="top" title="" data-original-title="Quack!">
-                     <a href="/profile/<?= $news->author['username'] ?>"><?= $news->author['username'] ?></a>                        
+                     <a href="/profile/<?= $news->author->username ?>"><?= $news->author->username ?></a>                        
                   </div>
                </div>
             </div>
@@ -27,10 +27,10 @@
             <?php foreach($news->reacties as $reacties) { ?>
             <div class="content-box">
                <div class="content-header">
-                  <div class="user-avatar" style="background-image: url(https://avatar.habnet.nl/avatarimage.php?figure=hr-<?= $reacties->user['look'] ?>&amp;head_direction=3&amp;action=wav)"></div>
+                  <div class="user-avatar" style="background-image: url(https://avatar.habnet.nl/avatarimage.php?figure=hr-<?= $reacties->user->look ?>&amp;head_direction=3&amp;action=wav)"></div>
                   <h1>
                      <a href="https://habnet.nl/profiel/David"><img class="user-icon"></a>
-                     <a href="https://habnet.nl/profiel/David"><font style="vertical-align: inherit;"><?= $reacties->user['username'] ?></font></a>
+                     <a href="https://habnet.nl/profiel/David"><font style="vertical-align: inherit;"><?= $reacties->user->username ?></font></a>
                      <p><font style="vertical-align: inherit;"><font style="vertical-align: inherit;"></font></font></p>
                      <div class="content-body"><font style="vertical-align: inherit;">
                        <?= $reacties->message ?><br><br>
